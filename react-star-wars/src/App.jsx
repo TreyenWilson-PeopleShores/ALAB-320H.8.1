7bb9691c9670659726148c713bc853976ade6d0c
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-24 09:39:45 -0500

    Connected main.jsx and App.jsx

--- react-star-wars/src/App.jsx
+++ react-star-wars/src/App.jsx
@@ -1,1 +1,11 @@
 // REPLACED index.js with main.jsx, as REACT errored without main.jsx
+
+function App(){
+    return(
+        <>
+        <h1>Hello!</h1>
+        </>
+    )
+}
+
+export default App
