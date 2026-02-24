9d0b91ad2990a0a32d177401edd32dd500e2f5a5
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-24 09:46:08 -0500

    Connected API file to App

--- react-star-wars/src/App.jsx
+++ react-star-wars/src/App.jsx
@@ -1,9 +1,12 @@
 // REPLACED index.js with main.jsx, as REACT errored without main.jsx
+import API from "./services/sw-api.jsx"
+
 
 function App(){
     return(
         <>
         <h1>Hello!</h1>
+        <API/>
         </>
     )
 }
---
+++ react-star-wars/src/services/sw-api.jsx
@@ -0,0 +1,10 @@
+const base_url = "https://swapi.dev/api/starships/9/"
+
+
+function API(){
+    return (
+        <p>API connected!</p>
+    )
+}
+
+export default API
